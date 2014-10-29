5b3ec0bb8718af9e57390919f5088f1ddbbeebf5
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2014-10-29 13:51:09 -0700

    simplify devise User routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
 
   resources :clients
-
-  devise_for :users
   
   get 'about' => 'welcome#about'
 
