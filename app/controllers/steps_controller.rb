743532138f5eb03241a6198d6f39a68abfe7a003
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2014-11-02 17:37:29 -0800

    Fix buggy step and text partial. Re-do new and edit forms for Steps.

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -6,10 +6,9 @@ class StepsController < ApplicationController
 
   def create
     @goal = Goal.find(params[:goal_id])
-    action_plan_id = @goal.action_plan_id
-    @action_plan = ActionPlan.find(action_plan_id)
-    
+    @action_plan = ActionPlan.find(@goal.action_plan_id)
     @step = @goal.steps.build(step_params)
+
     if @step.save
       flash[:notice] = "Success!  Step was saved."
       redirect_to @action_plan
@@ -20,9 +19,21 @@ class StepsController < ApplicationController
   end
 
   def edit
+    @step = Step.find(params[:id])
   end
 
   def update
+    @step = Step.find(params[:id])
+    @goal = Goal.find(@step.goal_id)
+    @action_plan = ActionPlan.find(@goal.action_plan_id)
+
+    if @step.update_attributes(step_params)
+      flash[:notice] = "Success!  Step was updated."
+      redirect_to @action_plan
+    else
+      flash[:error] = "There was an error saving the step. Please try again."
+      render :edit
+    end
   end
 
 
