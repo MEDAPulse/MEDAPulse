6a6c2bce8b6f4b54014f5667aecb788890572b20
Brittany Martin <brittany.jill.martin@gmail.com>
2014-11-06 12:20:45 -0800

    Add central Pulse logo and cleaned up the header of extra links

---
+++ app/controllers/text_messages_controller.rb
@@ -0,0 +1,37 @@
+class TextMessagesController < ApplicationController
+  
+  def new
+    @client = current_user.clients.find(params[:client_id])
+    @text_message = @client.text_messages.build
+  end
+
+  def create
+    @client = Client.find(params[:text_message][:client_id])
+    content = params[:text_message][:content]
+    message_sent = Twilio.send_message(@client.phone, content)
+    
+    if message_sent
+      text_message = TextMessage.new(text_message_params)
+      text_message.incoming_message = false
+      if text_message.save
+        flash[:notice] = "Success! Text Message was saved."
+        redirect_to @action_plan
+      else
+        flash[:error] = "There was an error saving the text message. Please try again."
+        render :new
+      end
+    end
+
+  def edit
+    @text_message = TextMessage.find(params[:id])
+  end
+
+  end  
+
+  private
+
+  def text_message_params
+    params.require(:text_message).permit(:content, :client_id)
+  end  
+
+end
