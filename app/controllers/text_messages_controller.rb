aedf2ec6aa8826b00baf1ae5ff25b7c2edf77184
Brittany Martin <brittany.jill.martin@gmail.com>
2014-11-12 20:28:34 -0800

    the beauty that is text messaging working for real time texts

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -1,41 +1,42 @@
 class TextMessagesController < ApplicationController
   
-  def new
-    @client = current_user.clients.find(params[:client_id])
-    @text_message = @client.text_messages.build
-  end
+def new
+  @step = Step.find(params[:step_id])
+  @text_message = @step.text_messages.build
+end
 
-  def create
-    @client = current_user.clients.find(params[:client_id])
-    content = params[:text_message][:content]
+def create
+  @step = Step.find(params[:step_id])
+  content = params[:text_message][:content]
     
-    @text_message = @client.text_messages.build(text_message_params)
-    @text_message.incoming_message = false
-    @text_message.sentstatus = false
+  @text_message = @step.text_messages.build(text_message_params)
+  @text_message.incoming_message = false
+  @text_message.sentstatus = false
 
-    if @text_message.save
-      flash[:notice] = "Success! Text Message was saved."
-      @text_message.send_text_message(@text_message.content)
-      redirect_to @client  
-    else
-      flash[:error] = "There was an error saving the text message. Please try again."
-      render :new
+  if @text_message.save
+    flash[:notice] = "Success! Text Message was saved."
+    @text_message.send_text_message(@text_message.content)
+    redirect_to @text_message.step.goal.action_plan 
+  else
+    flash[:error] = "There was an error saving the text message. Please try again."
+    render :new
     end
+end
+
+def update
+  if @text_message.update_attributes(text_message_params)
+      flash[:notice] = "Success! Text was updated."
+      redirect_to @text_message.step.goal.action_plan
+    else
+      flash[:error] = "There was an error saving the text. Please try again."
+      render :edit
   end
+end 
 
-  # def update
- 
-    # @text_message = TextMessage.find(params[:id]) 
+private
 
-   #if @text_message.sentstatus == true
-      # @text_message.send_text_message
-  # end
-  # end 
-
-  private
-
-  def text_message_params
-    params.require(:text_message).permit(:content, :scheduled_date, :client_id, :sentstatus)
-  end  
+def text_message_params
+  params.require(:text_message).permit(:content, :scheduled_date, :client_id, :sentstatus)
+end  
 
 end
--- app/models/text_message.rb
+++ app/models/text_message.rb
@@ -2,10 +2,15 @@ class TextMessage < ActiveRecord::Base
 require 'twilio-ruby'
 
 belongs_to :client, dependent: :destroy
+belongs_to :step, dependent: :destroy
+
+before_save :grab_phone
+
+def grab_phone
+  self.phone = step.goal.action_plan.client.phone
+end
 
   def send_text_message(message)
-  
-    number_to_send_to = client.phone
 
     twilio_sid = ENV["TWILIO_ACCT_SID"]
     twilio_token = ENV["TWILIO_AUTH_TOKEN"]
@@ -15,7 +20,7 @@ belongs_to :client, dependent: :destroy
     
     @twilio_client.account.sms.messages.create(
       :from => "+1#{twilio_phone_number}",
-      :to => number_to_send_to,
+      :to => phone,
       :body => message)
   end  
 end
