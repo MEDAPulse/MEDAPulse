23420570b782250e20369e101dbd6777fa262b48
Brittany Martin <brittany.jill.martin@gmail.com>
2014-11-25 15:05:02 -0800

    Sending texts back to Sidekiq

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,8 @@ set :environment, "production"
 
 # 8pm UTC since the desired time is 12pm PST (Ninefold runs on UTC time)
 
-every :day, :at => '8pm' do 
+# every :day, :at => '8pm' do
+every 5.minutes do 
   rake "send_scheduled_text:texts"
 end
 
