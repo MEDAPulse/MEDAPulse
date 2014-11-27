e4c8a5ee7c25e07bd0515481e22e8c8d08822c04
Brittany Martin <brittany.jill.martin@gmail.com>
2014-11-27 12:27:13 -0800

    Modified the text message worker

--- app/workers/send_text_worker.rb
+++ app/workers/send_text_worker.rb
@@ -1,8 +1,10 @@
 class SendTextWorker
   include Sidekiq::Worker
+  sidekiq_options queue: :immediate
 
   def perform(text_message_id)
-    text_message = Text_Message.find(text_message_id)
-    SendText.new(text_message_id).send_text_message
+    text = TextMessage.find(text_message_id)
+    content = text.content
+    TextMessage.new.send_text_message(content)
   end
 end
