175e690db3b23236d45d7a61197723aa0b390c94
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2014-12-03 21:49:28 -0800

    Add confirmed users to Cucumber tests.

--- features/step_definitions/clients_steps.rb
+++ features/step_definitions/clients_steps.rb
@@ -1,6 +1,5 @@
 def create_coach_with_clients
-  @coach = FactoryGirl.create(:user_with_clients)
-  # @user.clients.each { |client| puts client.to_yaml }
+  @coach = FactoryGirl.create(:confirmed_user_with_clients)
 end
 
 When(/^create several clients$/) do
