684f3fd0c6fa5ea4ecc1f5a41f0ae5035fab0775
Brittany Martin <brittany.jill.martin@gmail.com>
2015-01-18 20:32:18 -0800

    Set up staging branch, will switch staging.medapulse.org to this env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'medapulse.org' }
+  config.action_mailer.default_url_options = { host: 'staging.medapulse.org' }
 end
