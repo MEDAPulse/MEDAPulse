e3f0482bf530f2403091a03f8a76dcb11774f210
Meredith Taylor-Tanaka <mlttanaka@mochipuff.net>
2015-02-25 20:08:16 -0800

    Add todos.

--- features/step_definitions/clients_steps.rb
+++ features/step_definitions/clients_steps.rb
@@ -17,6 +17,9 @@ Then(/^I should see my list of clients\.$/) do
   @clients = @coach.clients
   expect(current_path).to eq("/clients")
   expect(@clients.count).to eq(5)
+  # TODO Angular test - Seach existing clients should work.
+  # TODO Angular test - 'Showing #n clients' should show the right number of clients.
+  # TODO Angular test - Name, Email, Phone, MEDA Contact ID, and Salesforce ID columns should be filled with data.
 end
 
 When(/^click on an individual client that has actions plans, goals, and steps$/) do
