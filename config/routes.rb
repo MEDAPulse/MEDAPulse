9a8fc4cec428889a695c9d3769b4a99e587912db
Brittany Martin <brittany.jill.martin@gmail.com>
2015-03-02 11:53:22 -0800

    Remove the Sidekiq web component from the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-require 'sidekiq/web'
-
   devise_for :users, :path => '',
     :path_names => {:sign_in => 'login', :sign_out => 'logout'}, 
     :controllers => { registrations: 'registrations' }
